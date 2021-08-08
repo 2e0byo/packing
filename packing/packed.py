9e1ffa105a29f748545e59644de3c379955c36b5
John Maximilian <2e0byo@gmail.com>
2021-08-08 10:55:52 +0200

    refactor massively

--- packing/packed.py
+++ packing/packed.py
@@ -1,52 +1,87 @@
 import struct
 import math
 from .util import pack_bools, unpack_bools
-from text import RotatingLog
+from .text import RotatingLog, nofileerror
+from collections import namedtuple
+
+Line = namedtuple("line", ("floats", "ints", "bools"))
 
 
 class PackedRotatingLog(RotatingLog):
-    def __init__(self, name, outdir, floats, bools, **kwargs):
+    def __init__(self, name, outdir, floats, ints, bools, **kwargs):
         self.floats = floats
         self.bools = bools
+        self.ints = ints
         self.ext = "bin"
-        self.bool_bytes = math.ceil(bools / 8)
-        self.line_size = self.bool_bytes + len(struct.pack("f", 99.78)) * floats
+        self.line_size = (
+            self.bool_bytes
+            + len(struct.pack("f", 99.78)) * floats
+            + len(struct.pack("i", 123678)) * ints
+        )
         super().__init__(name, outdir, **kwargs)
 
-    def pack(self, floats=None, bools=None):
+    @property
+    def bool_bytes(self):
+        return math.ceil(self.bools / 8)
+
+    @property
+    def float_bytes(self):
+        return len(struct.pack("f", 99.78) * self.floats)
+
+    @property
+    def int_bytes(self):
+        return len(struct.pack("i", 12346) * self.ints)
+
+    @property
+    def line_size(self):
+        return self.bool_bytes + self.float_bytes + self.int_bytes
+
+    @property
+    def struct_string(self):
+        return "f" * self.floats + "i" * self.ints + "B" * self.bool_bytes
+
+    def pack(self, floats=None, ints=None, bools=None):
         if self.bool_bytes:
             bools = [
                 pack_bools(bools[i : min(i + 8, len(bools))])
                 for i in range(0, len(bools), 8)
             ]
 
-        args = []
+            args = []
+
+        # micropython only allows one * expansion per line
         if floats:
             args += floats
         if bools:
             args += bools
-        packed = struct.pack("f" * self.floats + "B" * self.bool_bytes, *args)
+        if ints:
+            args += ints
+        packed = struct.pack(self.pack_string, *args)
         return packed
 
     def unpack(self, packed):
-        unpacked = struct.unpack("f" * self.floats + "B" * self.bool_bytes, packed)
+        unpacked = struct.unpack(self.pack_string, packed)
+        bools, ints, floats = (), (), ()
         if self.bools:
-            floats = unpacked[: -self.bool_bytes]
             bools = []
             for byte in unpacked[-self.bool_bytes :]:
                 bools += unpack_bools(byte)
-            bools = bools[: self.bools]
-        else:
-            floats = unpacked
-            bools = []
-        return floats, bools
+            bools = tuple(bools)
+        if self.ints:
+            ints = unpacked[self.float_bytes : self.float_bytes + self.int_bytes]
+        if self.floats:
+            floats = unpacked[self.float_bytes]
+        return Line(floats, ints, bools)
 
     def rotate_logs(self):
         super().rotate_logs()
         self.pos = 0
 
-    def writeln(self, line):
+    def append(self, floats=None, bools=None):
         line = self.pack(floats, bools)
+        super().append(line)
+
+    def writeln(self, line):
         with open(self.logf(), "ab") as f:
             f.write(line)
 
@@ -62,62 +97,3 @@ class PackedRotatingLog(RotatingLog):
                     self._to_read -= 1
         except nofileerror:
             pass
-
-
-class CachingPackedRotatingLog(PackedRotatingLog):
-    def __init__(self, name, outdir, floats, bools, buffer_size=100, **kwargs):
-        self.buffer_size = buffer_size
-        self.buf = bytearray(self.line_size * buffer_size)
-        super().__init__(name, outdir, floats, bools, **kwargs)
-
-    def append(self, floats=None, bools=None):
-        if self.buffer_pos == 0 and self.pos:
-            self.writeln()
-        pos = self.buffer_pos * self.line_size
-        self.buf[pos : pos + self.line_size] = self.pack(floats, bools)
-
-        if self.pos == self.log_size:
-            self.rotate_logs()
-
-        self.pos += 1
-
-    def read(self, logf=None, n=None, skip=0):
-        if not n:
-            n = self.log_size if logf else self.pos
-        self._to_read = n
-
-        if logf:
-            skip = self.log_size - n - skip
-            yield from self._reader(logf, skip)
-            return
-
-        rows_in_f = self.pos - self.buffer_pos
-        f_rows_needed = n + skip - self.buffer_pos
-
-        if f_rows_needed > rows_in_f:
-            # partial file is full file
-            other_rows = f_rows_needed - rows_in_f
-            fs, partial_f_rows = divmod(other_rows, self.log_size)
-            if skip:
-                skip = self.log_size - partial_f_rows
-                fs += 1
-        elif f_rows_needed:
-            fs = 0
-            skip = rows_in_f - f_rows_needed
-            partial_f_rows = f_rows_needed
-
-        if f_rows_needed:
-            yield from self._reader(self.logf(fs), skip)
-            skip = 0
-            for i in range(fs - 1, -1, -1):
-                yield from self._reader(self.logf(i), skip)
-
-        # read from ram
-        i = 0
-        while i < self._to_read:
-            pos = (skip + i) * self.line_size
-            region = self.buf[pos : pos + self.line_size]
-            if not region or i == self.buffer_pos:
-                break
-            yield self.unpack(region)
-            i += 1
