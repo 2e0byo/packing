d1c70ca9c4fece09f788553ca9bb92b66b2feba2
John Maximilian <2e0byo@gmail.com>
2021-08-08 10:56:10 +0200

    tests for ints too

--- tests/test_packed.py
+++ tests/test_packed.py
@@ -5,7 +5,7 @@ import pytest
 
 @pytest.fixture
 def packer(tmp_path):
-    p = PackedRotatingLog("log", str(tmp_path), 2, 8, log_lines=10)
+    p = PackedRotatingLog("log", str(tmp_path), 2, 2, 8, log_lines=10)
     yield p, tmp_path
 
 
@@ -13,7 +13,8 @@ def packer(tmp_path):
 def equal():
     def eq(exp, resp):
         for eline, rline in zip(exp, resp):
-            assert eline[1] == rline[1], "Bools not equal"
+            assert eline[2] == rline[2], "Bools not equal"
+            assert eline[1] == rline[1], "Ints not equal"
             for i, x in enumerate(eline[0]):
                 assert rline[0][i] == pytest.approx(x)
         return True
@@ -22,12 +23,12 @@ def equal():
 
 
 def test_equal(equal):
-    a = [[[9, 8, 8], [True, False]]]
-    b = [[[9, 8, 8], [False, True]]]
+    a = [[[9, 8, 8], [8, 9], [True, False]]]
+    b = [[[9, 8, 8], [8, 9], [False, True]]]
     assert equal(a, a)
     with pytest.raises(AssertionError):
         assert equal(a, b)
-    c = [[[9, 9, 8], [True, False]]]
+    c = [[[9, 9, 8], [8, 9], [True, False]]]
     with pytest.raises(AssertionError):
         assert equal(a, c)
 
