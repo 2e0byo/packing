cb8ddcce143aa36f96f8a02756b4711094073ade
John Maximilian <2e0byo@gmail.com>
2021-08-08 17:49:30 +0200

    move fixture

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+@pytest.fixture
+def equal():
+    def eq(exp, resp):
+        for eline, rline in zip(exp, resp):
+            print(eline)
+            assert tuple(eline[2]) == rline[2], "Bools not equal"
+            assert tuple(eline[1]) == rline[1], "Ints not equal"
+            for i, x in enumerate(eline[0]):
+                assert rline[0][i] == pytest.approx(x)
+        return True
+
+    yield eq
--- tests/test_packed.py
+++ tests/test_packed.py
@@ -9,20 +9,6 @@ def packer(tmp_path):
     yield p, tmp_path
 
 
-@pytest.fixture
-def equal():
-    def eq(exp, resp):
-        for eline, rline in zip(exp, resp):
-            print(eline)
-            assert tuple(eline[2]) == rline[2], "Bools not equal"
-            assert tuple(eline[1]) == rline[1], "Ints not equal"
-            for i, x in enumerate(eline[0]):
-                assert rline[0][i] == pytest.approx(x)
-        return True
-
-    yield eq
-
-
 def test_equal(equal):
     a = [((9, 8, 8), (8, 9), (True, False))]
     b = [((9, 8, 8), (8, 9), (False, True))]
