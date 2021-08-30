1b5010646b47985f620b76f2aa74beee3951715a
John Maximilian <2e0byo@gmail.com>
2021-08-30 14:11:40 +0100

    tidy

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,6 @@ def equal():
     def eq(exp, resp):
         assert len(exp) == len(resp), "Different lengths"
         for eline, rline in zip(exp, resp):
-            print(eline)
             assert tuple(eline[2]) == rline[2], "Bools not equal"
             assert tuple(eline[1]) == rline[1], "Ints not equal"
             for i, x in enumerate(eline[0]):
