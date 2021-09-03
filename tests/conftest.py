e4b3dd761bc9175455d0071ac5d7cbae2a57026f
John Maximilian <2e0byo@gmail.com>
2021-09-03 19:11:32 +0100

    handle ids

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,10 +6,11 @@ def equal():
     def eq(exp, resp):
         assert len(exp) == len(resp), "Different lengths"
         for eline, rline in zip(exp, resp):
-            assert tuple(eline[2]) == rline[2], "Bools not equal"
-            assert tuple(eline[1]) == rline[1], "Ints not equal"
-            for i, x in enumerate(eline[0]):
-                assert rline[0][i] == pytest.approx(x)
+            assert eline[0] == rline[0], "Index not equal"
+            assert tuple(eline[3]) == rline[3], "Bools not equal"
+            assert tuple(eline[2]) == rline[2], "Ints not equal"
+            for i, x in enumerate(eline[1]):
+                assert rline[1][i] == pytest.approx(x)
         return True
 
     yield eq
